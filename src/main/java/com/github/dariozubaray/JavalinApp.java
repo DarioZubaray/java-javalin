d0cb7601aa92408bf1127c4dcffb14e92de714d0
dario.zubaray <dzubaray@imagosur.com>
2019-06-25 12:32:35 -0300

    definiendo las rutas de usuario en el archivo main

--- src/main/java/com/github/dariozubaray/JavalinApp.java
+++ src/main/java/com/github/dariozubaray/JavalinApp.java
@@ -1,5 +1,6 @@
 package com.github.dariozubaray;
 
+import com.github.dariozubaray.model.user.UserController;
 import io.javalin.Javalin;
 
 public class JavalinApp {
@@ -7,6 +8,8 @@ public class JavalinApp {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.get("/hola", ctx -> ctx.html("Hello from Javalin!"));
+        app.get("/users", UserController.fetchAllUsernames);
+        app.get("/users/:id", UserController.fetchById);
     }
 
 }
